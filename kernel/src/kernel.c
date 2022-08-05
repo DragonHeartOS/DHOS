bf264eb483644c54451622abcfa17701b48ff782
Ian Moffett <spurious.vector@gmail.com>
2022-08-05 15:14:38 -0400

    Feat: Add Logging.

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,11 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <limine.h>
-
-static volatile struct limine_terminal_request terminal_request = {
-    .id = LIMINE_TERMINAL_REQUEST,
-    .revision = 0
-};
+#include <common/log.h>
 
 static void done(void) {
     for (;;) {
@@ -14,13 +10,7 @@ static void done(void) {
 }
 
 void _start(void) {
-    if (terminal_request.response == NULL
-     || terminal_request.response->terminal_count < 1) {
-        done();
-    }
-
-    struct limine_terminal *terminal = terminal_request.response->terminals[0];
-    terminal_request.response->write(terminal, "Hello World", 11);
+    kprintf(KINFO "Hello, World!\n");
 
     done();
 }
