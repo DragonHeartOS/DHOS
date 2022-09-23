c0a1ef0c8ea187920fac32201da15694c00d06b9
slendi <slendi@socopon.com>
2022-09-23 16:30:46 +0300

    Kernel: Make IDTR not padded for TCC

--- kernel/interrupts/IDT.h
+++ kernel/interrupts/IDT.h
@@ -18,12 +18,18 @@ typedef struct {
   u32 reserved;
 } interrupt_gate_descriptor_t;
 
+#if defined(__TINYC__)
+#pragma pack(1)
+#endif
+
 typedef struct PACKED {
-  //  u16 limit PACKED; // Size.
-  //  u64 base PACKED;  // Base address.
   u16 limit; // Size.
   u64 base;  // Base address.
 } idt_ptr_t;
+
+#if defined(__TINYC__)
+#pragma pack(1)
+#endif
 
 void idt_set_descriptor(u8 vector, void *isr, u8 flags);
 void idt_install(void);
